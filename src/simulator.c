cd54688359505b28f003349c61be31daec0d459c
Andrei Florea <andrei91ro@gmail.com>
2016-03-20 21:17:53 +0200

    initially print programs alongside obj contents of agents

--- src/simulator.c
+++ src/simulator.c
@@ -27,12 +27,40 @@ char* printMultisetObj(multiset_obj_t *multiset) {
     return outputBuffer;
 }
 
-void printColonyState(Pcolony_t *pcol) {
+char* printProgram(Program_t *program) {
+    memset(outputBuffer, '\0', 255);
+    for (uint8_t i = 0; i < program->nr_rules; i++)
+        //if this is a non-conditional rule
+        if (program->rules[i].type < RULE_TYPE_CONDITIONAL_EVOLUTION_EVOLUTION)
+            // x -> y
+            sprintf(outputBuffer, "%s %s%s%s, ", outputBuffer,
+                    objectNames[program->rules[i].lhs],
+                    ruleNames[program->rules[i].type],
+                    objectNames[program->rules[i].rhs]);
+        else
+            // x -> y / z -> w
+            sprintf(outputBuffer, "%s %s%s%s / %s%s%s, ", outputBuffer,
+                    objectNames[program->rules[i].lhs],
+                    ruleNames[lookupFirst[program->rules[i].type]],
+                    objectNames[program->rules[i].rhs],
+                    // alternative fields
+                    objectNames[program->rules[i].alt_lhs],
+                    ruleNames[lookupSecond[program->rules[i].type]],
+                    objectNames[program->rules[i].alt_rhs]);
+    
+    return outputBuffer;
+}
+
+void printColonyState(Pcolony_t *pcol, bool with_programs) {
 
     printf("\n    Pcolony.env = [%s]", printMultisetEnv(&pcol->env));
     printf("\n    Pswarm.env = [%s]", printMultisetEnv(&pcol->pswarm.global_env));
-    for (uint8_t i = 0; i < pcol->nr_agents; i++)
+    for (uint8_t i = 0; i < pcol->nr_agents; i++) {
         printf("\n    %s.obj = [%s]", agentNames[i], printMultisetObj(&pcol->agents[i].obj));
+        if (with_programs)
+            for (uint8_t prg_nr = 0; prg_nr < pcol->agents[i].nr_programs; prg_nr++)
+                printf("\n        P%d = < %s >", prg_nr, printProgram(&pcol->agents[i].programs[prg_nr]));
+    }
 }
 
 int main(int argc, char **argv) {
@@ -44,11 +72,11 @@ int main(int argc, char **argv) {
     lulu_init(&pcol);
 
     printi(("Initial configuration:"));
-    printColonyState(&pcol);
+    printColonyState(&pcol, TRUE);
 
     expand_pcolony(&pcol, 71);
     printi(("Configuration after wildcard expansion:"));
-    printColonyState(&pcol);
+    printColonyState(&pcol, TRUE);
 
     while (1) {
         sim_step_result_t result = SIM_STEP_RESULT_FINISHED;
@@ -57,7 +85,7 @@ int main(int argc, char **argv) {
 
         result = pcolony_runSimulationStep(&pcol);
 
-        printColonyState(&pcol);
+        printColonyState(&pcol, FALSE);
 
         if (result == SIM_STEP_RESULT_NO_MORE_EXECUTABLES) {
             printi(("Simulation finished sucesfully"));
